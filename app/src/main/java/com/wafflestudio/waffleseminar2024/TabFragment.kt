84dc6e41d29a205bc1a45611158569fa8c1c9726
hjlim7831 <hjlim7831@gmail.com>
2024-10-16 22:22:31 +0900

    add: 장르 아이콘 리소스 파일 추가

--- app/src/main/java/com/wafflestudio/waffleseminar2024/TabFragment.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/TabFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.wafflestudio.waffleseminar2024.databinding.TabMovieViewBinding
 import com.wafflestudio.waffleseminar2024.search.GenreDTO
+import com.wafflestudio.waffleseminar2024.search.SearchItem
 import com.wafflestudio.waffleseminar2024.search.movieGenres
 import kotlinx.serialization.json.Json
 
@@ -34,7 +35,34 @@ class MovieFragment: Fragment() {
         return binding.root
     }
 
-    private fun parseMovieGenres(): GenreDTO {
-        return Json.decodeFromString(movieGenres)
+    private fun genreButtons(): List<SearchItem.GenreButton> {
+        val genreDTO = Json.decodeFromString<GenreDTO>(movieGenres)
+        return genreDTO.genres.map { genre ->
+            SearchItem.GenreButton(genre.name, genre.id, getIconResId(genre.name))
+        }
+    }
+
+    private fun getIconResId(genreName: String):Int {
+        return when (genreName) {
+            "액션" -> R.drawable.genre_icons_action
+            "모험" -> R.drawable.genre_icons_adventure
+            "애니메이션" -> R.drawable.genre_icons_animation
+            "코미디" -> R.drawable.genre_icons_comedy
+            "범죄" -> R.drawable.genre_icons_crime
+            "다큐멘터리" -> R.drawable.genre_icons_documentary
+            "드라마" -> R.drawable.genre_icons_drama
+            "가족" -> R.drawable.genre_icons_family
+            "판타지" -> R.drawable.genre_icons_fantasy
+            "역사" -> R.drawable.genre_icons_history
+            "공포" -> R.drawable.genre_icons_horror
+            "음악" -> R.drawable.genre_icons_music
+            "미스터리" -> R.drawable.genre_icons_mystery
+            "SF" -> R.drawable.genre_icons_sf
+            "TV 영화" -> R.drawable.genre_icons_tv
+            "스릴러" -> R.drawable.genre_icons_thriller
+            "전쟁" -> R.drawable.genre_icons_war
+            "서부" -> R.drawable.genre_icons_western
+            else -> R.drawable.movie_recorder
+        }
     }
 }
--- app/src/main/java/com/wafflestudio/waffleseminar2024/search/SearchItem.kt
+++ app/src/main/java/com/wafflestudio/waffleseminar2024/search/SearchItem.kt
@@ -2,5 +2,5 @@ package com.wafflestudio.waffleseminar2024.search
 
 sealed class SearchItem {
     data class Label(val name: String): SearchItem()
-    data class GenreButton(val name: String, val iconResId: Int): SearchItem()
+    data class GenreButton(val name: String, val genreId:Int, val iconResId: Int): SearchItem()
 }
